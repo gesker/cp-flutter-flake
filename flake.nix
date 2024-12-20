10523ae9b9b112a8e613d54db3e8ecb110dd5335
gesker <dennis@gesker.com>
2024-12-20 10:48:16 -0700

    put mine in hello

--- flake.nix
+++ flake.nix
@@ -4,8 +4,11 @@
   # Nixpkgs / NixOS version to use.
   inputs.nixpkgs.url = "nixpkgs/nixos-21.05";
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
+
+      inherit nixpkgs;
 
       # to work with older version of flakes
       lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
@@ -14,14 +17,34 @@
       version = builtins.substring 0 8 lastModifiedDate;
 
       # System types to support.
-      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      supportedSystems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
 
       # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
 
       # Nixpkgs instantiated for supported system types.
-      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; overlays = [ self.overlay ]; });
+      nixpkgsFor = forAllSystems (
+        system:
+        import nixpkgs {
+          inherit system;
+          overlays = [ self.overlay ];
+        }
+      );
 
+      FLUTTER_VERSION = "3.27.1";
+      FILENAME = "flutter_linux_${FLUTTER_VERSION}-stable.tar.xz";
+      FETCH_URL = "https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/${FILENAME}";
+
+      flutterArchive = nixpkgs.fetchurl {
+        url = "${FETCH_URL}";
+        sha256 = "sha256-YUl+tkzXs6qZypkRzNkhyNq3mv2QnKHJ/5VGBn689so=";
+        executable = false;
+      };
     in
 
     {
@@ -29,99 +52,90 @@
       # A Nixpkgs overlay.
       overlay = final: prev: {
 
-        hello = with final; stdenv.mkDerivation rec {
-          name = "hello-${version}";
+        flutter =
+          with final;
+          stdenv.mkDerivation rec {
+            name = "flutter-${version}";
 
-          unpackPhase = ":";
-
-          buildPhase =
-            ''
-              cat > hello <<EOF
-              #! $SHELL
-              echo "Hello Nixers!"
-              EOF
-              chmod +x hello
+            unpackPhase = ":";
+            buildPhase = ''
+              tar -xvf "${flutterArchive}"
             '';
+            installPhase = "mkdir -p $out; cp -ar flutter/* $out";
 
-          installPhase =
-            ''
-              mkdir -p $out/bin
-              cp hello $out/bin/
-            '';
-        };
+          };
 
       };
 
       # Provide some binary packages for selected system types.
-      packages = forAllSystems (system:
-        {
-          inherit (nixpkgsFor.${system}) hello;
-        });
+      packages = forAllSystems (system: {
+        inherit (nixpkgsFor.${system}) flutter;
+      });
 
       # The default package for 'nix build'. This makes sense if the
       # flake provides only one package or there is a clear "main"
       # package.
-      defaultPackage = forAllSystems (system: self.packages.${system}.hello);
+      defaultPackage = forAllSystems (system: self.packages.${system}.flutter);
 
       # A NixOS module, if applicable (e.g. if the package provides a system service).
-      nixosModules.hello =
+      nixosModules.flutter =
         { pkgs, ... }:
         {
           nixpkgs.overlays = [ self.overlay ];
 
-          environment.systemPackages = [ pkgs.hello ];
+          environment.systemPackages = [ pkgs.flutter ];
 
           #systemd.services = { ... };
         };
 
-      # Tests run by 'nix flake check' and by Hydra.
-      checks = forAllSystems
-        (system:
-          with nixpkgsFor.${system};
+      # # Tests run by 'nix flake check' and by Hydra.
+      # checks = forAllSystems
+      #   (system:
+      #     with nixpkgsFor.${system};
 
-          {
-            inherit (self.packages.${system}) hello;
+      #     {
+      #       inherit (self.packages.${system}) hello;
 
-            # Additional tests, if applicable.
-            test = stdenv.mkDerivation {
-              name = "hello-test-${version}";
+      #       # Additional tests, if applicable.
+      #       test = stdenv.mkDerivation {
+      #         name = "hello-test-${version}";
 
-              buildInputs = [ hello ];
+      #         buildInputs = [ hello ];
 
-              unpackPhase = "true";
+      #         unpackPhase = "true";
 
-              buildPhase = ''
-                echo 'running some integration tests'
-                [[ $(hello) = 'Hello Nixers!' ]]
-              '';
+      #         buildPhase = ''
+      #           echo 'running some integration tests'
+      #           [[ $(hello) = 'Hello Nixers!' ]]
+      #         '';
 
-              installPhase = "mkdir -p $out";
-            };
-          }
+      #         installPhase = "mkdir -p $out";
+      #       };
+      #     }
 
-          // lib.optionalAttrs stdenv.isLinux {
-            # A VM test of the NixOS module.
-            vmTest =
-              with import (nixpkgs + "/nixos/lib/testing-python.nix") {
-                inherit system;
-              };
+      #     // lib.optionalAttrs stdenv.isLinux {
+      #       # A VM test of the NixOS module.
+      #       vmTest =
+      #         with import (nixpkgs + "/nixos/lib/testing-python.nix") {
+      #           inherit system;
+      #         };
 
-              makeTest {
-                nodes = {
-                  client = { ... }: {
-                    imports = [ self.nixosModules.hello ];
-                  };
-                };
+      #         makeTest {
+      #           nodes = {
+      #             client = { ... }: {
+      #               imports = [ self.nixosModules.hello ];
+      #             };
+      #           };
 
-                testScript =
-                  ''
-                    start_all()
-                    client.wait_for_unit("multi-user.target")
-                    client.succeed("hello")
-                  '';
-              };
-          }
-        );
+      #           testScript =
+      #             ''
+      #               start_all()
+      #               client.wait_for_unit("multi-user.target")
+      #               client.succeed("hello")
+      #             '';
+      #         };
+      #     }
+      #   );
 
     };
 }
